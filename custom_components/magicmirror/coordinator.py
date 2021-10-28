e946eaed24392d33e74412779aedbd0ec1351472
Sindre Broch <sindre.broch@nav.no>
2021-10-28 09:19:03 +0200

    Add configuration_url
    * Re-add update_available sensor
    * Use super()._handle_coordinator_update

--- custom_components/magicmirror/coordinator.py
+++ custom_components/magicmirror/coordinator.py
@@ -28,12 +28,13 @@ class MagicMirrorDataUpdateCoordinator(DataUpdateCoordinator):
         """Initialize."""
 
         self.api = api
-        self._attr_device_info: DeviceInfo = {
-            "name": "MagicMirror",
-            "model": "MagicMirror",
-            "manufacturer": "MagicMirror",
-            "identifiers": {(MAGICMIRROR_DOMAIN, "MagicMirror")},
-        }
+        self._attr_device_info = DeviceInfo(
+            name="MagicMirror",
+            model="MagicMirror",
+            manufacturer="MagicMirror",
+            identifiers={(MAGICMIRROR_DOMAIN, "MagicMirror")},
+            configuration_url="{api.base_url}/remote.html",
+        )
 
         super().__init__(
             hass,
@@ -48,15 +49,19 @@ class MagicMirrorDataUpdateCoordinator(DataUpdateCoordinator):
         try:
             async with timeout(10):
                 monitor: MonitorResponse = await self.api.monitor_status()
+                update: QueryResponse = await self.api.update_available()
                 brightness: QueryResponse = await self.api.get_brightness()
 
                 if not monitor.success:
                     LOGGER.warning("Failed to fetch monitor-status for MagicMirror")
+                if not update.success:
+                    LOGGER.warning("Failed to fetch update-status for MagicMirror")
                 if not brightness.success:
                     LOGGER.warning("Failed to fetch brightness for MagicMirror")
 
                 return {
                     Entity.MONITOR_STATUS.value: monitor.monitor,
+                    Entity.UPDATE_AVAILABLE.value: bool(update.result),
                     Entity.BRIGHTNESS.value: int(brightness.result),
                 }
 
