c8cb07d0a624c2c592f744b6aef1b668b85cd9eb
Sindre Broch <sindrebroch@gmail.com>
2022-04-06 17:38:21 +0200

    Fix loading and state for Update-entity

--- custom_components/magicmirror/update.py
+++ custom_components/magicmirror/update.py
@@ -1,8 +1,11 @@
-from homeassistant.components.update import UpdateEntity, UpdateEntityDescription
+"""Update for MagicMirror."""
+
+from homeassistant.components.update import UpdateEntity
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import EntityCategory
+from homeassistant.const import STATE_ON
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity import EntityCategory, EntityDescription
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
@@ -10,27 +13,28 @@ from custom_components.magicmirror.const import DOMAIN
 from custom_components.magicmirror.coordinator import MagicMirrorDataUpdateCoordinator
 from custom_components.magicmirror.models import Entity
 
-UPDATES: tuple[UpdateEntityDescription, ...] = (
-    UpdateEntityDescription(
-        key=Entity.UPDATE_AVAILABLE.value,
-        name="MagicMirror Update Available",
-        entity_category=EntityCategory.DIAGNOSTIC,
-    )
-)
+OLD_VERSION = "outdated"
+LATEST_VERSION = "latest"
 
 async def async_setup_entry(
     hass: HomeAssistant, 
     entry: ConfigEntry, 
     async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Set up the Pi-hole update entities."""
+    """Set up the MagicMirror update entities."""
     
     coordinator: MagicMirrorDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
-    async_add_entities(
-        MagicMirrorUpdate(coordinator, description)
-        for description in UPDATES
-    )
+    async_add_entities([
+        MagicMirrorUpdate(
+            coordinator,     
+            EntityDescription(
+                key=Entity.UPDATE_AVAILABLE.value,
+                name="MagicMirror Update Available",
+                entity_category=EntityCategory.DIAGNOSTIC,
+            )
+        ) 
+    ])
 
 class MagicMirrorUpdate(CoordinatorEntity, UpdateEntity):
 
@@ -39,8 +43,7 @@ class MagicMirrorUpdate(CoordinatorEntity, UpdateEntity):
     def __init__(
         self,
         coordinator: MagicMirrorDataUpdateCoordinator,
-        name: str,
-        description: UpdateEntityDescription,
+        description: EntityDescription,
     ) -> None:
         """Initialize update entity."""
 
@@ -49,8 +52,28 @@ class MagicMirrorUpdate(CoordinatorEntity, UpdateEntity):
         self.coordinator = coordinator
         self.entity_description = description
         
+        self.sensor_data = self.get_sensor_data()
+
         self._attr_unique_id = f"{description.name}"
         self._attr_device_info = coordinator._attr_device_info
 
         self._attr_title = description.name
         self._attr_release_url = "https://github.com/MichMich/MagicMirror/releases/latest"
+        
+        self._attr_latest_version = LATEST_VERSION
+
+    def get_sensor_data(self) -> bool:
+        """Get sensor data."""
+        state = self.coordinator.data.__getattribute__(self.entity_description.key)
+        return True if state == STATE_ON else False
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle data update."""
+        self.sensor_data = self.get_sensor_data()
+        super()._handle_coordinator_update()
+
+    @property
+    def installed_version(self) -> str:
+        """Version installed and in use."""
+        return OLD_VERSION if self.sensor_data else LATEST_VERSION
