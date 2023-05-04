4526e8556fc274202d005a6fb7be4f5fed9e150f
Sindre Broch <sindrebroch@users.noreply.github.com>
2023-05-04 10:53:12 +0200

    async_forward_entry_setups

--- custom_components/magicmirror/__init__.py
+++ custom_components/magicmirror/__init__.py
@@ -50,7 +50,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     await async_setup_notify(hass, entry)
 
